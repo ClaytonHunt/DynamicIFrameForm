514d57bfd0d130e85cddb518bbe465ec1b2b28dd
clayton.hunt <clayton.hunt@e-hps.com>
2014-08-12 16:55:06 -0400

    Added test to test that the tester is working

--- build/tests/templateEngineTests.js
+++ build/tests/templateEngineTests.js
@@ -1,3 +1,14 @@
 /**
  * Created by clayton.hunt on 8/12/2014.
  */
+(function() {
+    'use strict';
+
+    describe('Templating Engine', function() {
+        describe('In General', function() {
+            it('exists', function() {
+               expect(1).toBe(1);
+            });
+        });
+    });
+}());
