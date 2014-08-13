c20dca1b0b399de20f9ef1d4c1fa7998a8002db3
clayton.hunt <clayton.hunt@e-hps.com>
2014-08-13 09:10:20 -0400

    Added tests for defineAs and setting a template. Trying to use a functional style

--- build/tests/templateEngineTests.js
+++ build/tests/templateEngineTests.js
@@ -15,6 +15,41 @@
             it('is a function', function() {
                 expect(typeof module.Template).toBe('function');
             });
+
+            it('is a Template', function() {
+                var t = new module.Template();
+                expect(t instanceof module.Template).toBeTruthy();
+            });
+        });
+
+        describe('Define As', function() {
+            var t = null;
+            beforeEach(function() {
+                t = new module.Template();
+            });
+
+            it('exists', function() {
+                expect(t.defineAs).toBeDefined();
+            });
+
+            it('is a function', function() {
+                expect(typeof t.defineAs).toBe('function');
+            });
+        });
+
+        describe('Setting A Template', function() {
+            it('can set a template using the constructor', function() {
+                var t = new module.Template('test');
+
+                expect(t.template).toBe('test');
+            });
+
+            it('can set a template using defineAs', function() {
+                var t = new module.Template();
+                t = t.defineAs('test');
+
+                expect(t.template).toBe('test');
+            });
         });
     });
 }(Toolbox));
--- src/js/template.js
+++ src/js/template.js
@@ -6,7 +6,13 @@ var Toolbox = Toolbox || {};
 (function(module) {
     'use strict';
 
-    module.Template = function() {
-        
+    module.Template = function(template) {
+        this.template = template;
+    };
+
+    var p = module.Template.prototype;
+
+    p.defineAs = function(template) {
+        return new module.Template(template);
     };
 }(Toolbox));
